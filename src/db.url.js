a67d8168f87b118ec8d3a5afa27e23d959ea297e
Chandra Sidhardh <65617539+wrench141@users.noreply.github.com>
2025-05-01 14:45:17 +0530

    Update db.url.js

--- src/db.url.js
+++ src/db.url.js
@@ -1,3 +1,3 @@
-// const DB_URL = "https://quickstock-server.vercel.app"
-const DB_URL = "http://localhost:4000"
+const DB_URL = "https://quickstock-server.vercel.app"
+// const DB_URL = "http://localhost:4000"
 export default DB_URL
